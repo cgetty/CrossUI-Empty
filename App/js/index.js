99989c06bdd98229856f3e2ba97378a94a237f1a
cgetty <sjgetty@gmail.com>
2023-03-06 02:42:03 -0600

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,17 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button17")
+                .setLeft("11.428571428571429em")
+                .setTop("4.571428571428571em")
+                .setWidth("11.276190476190477em")
+                .setHeight("2.742857142857143em")
+                .setCaption("Button")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
